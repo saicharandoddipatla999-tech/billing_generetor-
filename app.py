95ac0f551fc2f6960988cb9803c95ebaa0680891
SAI CHARAN DODDIPATLA <saicharandoddipatla999@gmail.com>
2026-09-07 11:05:27 +0530

    Configure app to run on specified port

--- app.py
+++ app.py
@@ -219,4 +219,5 @@ def delete_invoice(invoice_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
